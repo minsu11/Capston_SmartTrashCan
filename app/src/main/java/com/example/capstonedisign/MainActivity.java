37a9562f24ab950670d7bb273d418805cc01bb27
ms <zmfjtnl1@naver.com>
2023-06-02 23:49:56 +0900

    commit example

--- app/src/main/java/com/example/capstonedisign/MainActivity.java
+++ app/src/main/java/com/example/capstonedisign/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 selected = true;
                 sended_num -= 1;
+                // 자바
             }
         });
 
